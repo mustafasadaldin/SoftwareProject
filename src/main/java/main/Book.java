d6e6d09c1582df89b113a1b02105a9867d644e4e
amroaboamro <amer.abuamer444@gmail.com>
2021-02-20 00:01:42 +0200

    test add book

--- src/main/java/main/Book.java
+++ src/main/java/main/Book.java
@@ -2,29 +2,30 @@ package main;
 import java.util.ArrayList;
 
 public class Book {
-	String title ;
-	String author ;
-	String signature;
-	String ISBN;
+	public String title ;
+	public	String author ;
+	public	String signature;
+	public	String ISBN;
 	public ArrayList<Book> books=new ArrayList<Book>();
 	public Book() {
 		this.title ="Unkown";
 		this.signature ="Unkown";
 		this.author ="Unkown";
+		this.ISBN="0";
 	}
-	public Book(String t,String a, String s,String isbn) {
-		this.title =t;
-		this.signature =s;
-		this.author =a;
-		this.ISBN=isbn;
-	}
+	
 
 	public boolean add(int login,String title, String author, String signature, String ISBN) {
 		if(login==0) {
 		return false;
 	}
 		else {
-			books.add(new Book(title,author,signature,ISBN));
+			Book b= new Book();
+			b.title=title;
+			b.author=author;
+			b.signature=signature;
+			b.ISBN=ISBN;
+			books.add(b);
 			return true;
 		}
 		
