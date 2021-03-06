9e5f63bcfb3b4baad34e3ae68aea78744f7b50e6
amroaboamro <amer.abuamer444@gmail.com>
2021-03-06 22:48:56 +0200

    final version (unique signature checked)

--- src/test/java/test/AddBooksteps.java
+++ src/test/java/test/AddBooksteps.java
@@ -15,10 +15,28 @@ public Admin a;
 	
 	this.a = a;
 }
+	
 	@Given("the admin logged in")
-	public void the_admin_logged_in() {
+	public void the_admin_logged_in(io.cucumber.datatable.DataTable dt) {
 	    a.login=1;
 	    a.logout=0;
+	    
+	    Book.books.add(new Book());
+	    Book.books.add(new Book());
+	    Book.books.get(0).title=dt.cell(0, 0);
+	    Book.books.get(0).author=dt.cell(0, 1);
+	    Book.books.get(0).signature=dt.cell(0, 2);
+	    Book.books.get(0).ISBN=dt.cell(0, 3);
+	    
+	    Book.books.get(1).title=dt.cell(1, 0);
+	    Book.books.get(1).author=dt.cell(1, 1);
+	    Book.books.get(1).signature=dt.cell(1, 2);
+	    Book.books.get(1).ISBN=dt.cell(1, 3);
+
+	    
+	    
+	    
+	 
 	}
 
 	@When("the admin adds a book with title, author ,signature and ISBN")
--- use_cases/AddBook.feature
+++ use_cases/AddBook.feature
@@ -1,6 +1,8 @@
 Feature: Add book
 Scenario: Add a book when the administrator is logged in
 Given the admin logged in 
+|web2021|mustafa|must|1234567896|
+|web26545641|amr|am22|1234567812|
 When the admin adds a book with title, author ,signature and ISBN 
 Then the book will be added to the system
 
